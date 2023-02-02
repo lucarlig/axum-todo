2b0a45a0b9d3872a7e782e65be7d67f638ebb538
lucarlig <luca.carlig@stacktical.com>
2023-02-02 11:01:34 +0700

    axum hello world working locallly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{response::Html, routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(handler));
+
+    // run it
+    let addr: SocketAddr = "[::]:8080".parse().unwrap();
+    println!("listening on {}", addr.to_string());
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn handler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
