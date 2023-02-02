e1ae6787acd8fd23fc062b12929e3bc8f566bba6
lucarlig <luca.carlig@stacktical.com>
2023-02-02 16:19:43 +0700

    add sea orm

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 mod routes;
+use dotenvy::dotenv;
+use dotenvy_macro::dotenv;
+use sea_orm::Database;
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let database_uri: &str = dotenv!("DATABASE_URL");
+    let database = Database::connect(database_uri).await;
+
     let app = routes::create_router();
     let addr: SocketAddr = "[::]:8080".parse().unwrap();
 
